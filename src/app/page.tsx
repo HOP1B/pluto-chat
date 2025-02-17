9f8155b14021d95f4cc92c61f550cba29e7e8358
= <=>
2025-02-17 18:43:22 +0800

    feat: implementing ably
    Haven't finished so gonna get home and continue.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+
+// import * as Ably from 'ably';
+// import { AblyProvider, ChannelProvider } from 'ably/react';
+
+// const client = new Ably.Realtime({key: process.env.ABLY_API_KEY});
+
 export default function Home() {
   return (
+    // <AblyProvider client={client}>
+    //   <ChannelProvider channelName='main-chat'>
+    //   </ChannelProvider>
+    // </AblyProvider>
     <div>hi lol</div>
   );
 }
