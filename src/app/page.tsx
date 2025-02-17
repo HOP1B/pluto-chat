fdd77f3af532d9c55da689b2987a7d272486be5f
= <=>
2025-02-17 18:43:22 +0800

    feat: implementing ably
    Haven't finished so gonna get home and continue.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,20 @@
 
 import { TrippyBackground } from "./components/Common/TrippyBackground";
 
+// import * as Ably from 'ably';
+// import { AblyProvider, ChannelProvider } from 'ably/react';
+
+// const client = new Ably.Realtime({key: process.env.ABLY_API_KEY});
+
 export default function Home() {
   return (
     <div>
       <p>hi lol</p>
       <TrippyBackground></TrippyBackground>
-    </div>
-  );
+      {/* <AblyProvider client={client}>
+      <ChannelProvider channelName='main-chat'>
+      </ChannelProvider>
+      </AblyProvider>
+      <div>hi lol</div> */}
+    </div>)
 }
