54ee6a200cbaab1e37141445b9b82df71e477a07
= <=>
2025-02-18 20:32:55 +0800

    feat: implemented base form chatting
    Yes real time chatting is possible now. Altough it doesn't save messages yet it's fine for now.
    Actually no it's not. Now I recommend you create a new issue on user authentication so we could
    continue this further. What I mean is right now we need the database for handling messages and to do
    that we depend on user and user id.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 "use client";
 
-// import * as Ably from 'ably';
-// import { AblyProvider, ChannelProvider } from 'ably/react';
-
-// const client = new Ably.Realtime({key: process.env.ABLY_API_KEY});
+import dynamic from "next/dynamic";
+const Chat = dynamic(
+  () => import("./components/chat/Chat").then((mod) => mod.Chat),
+  {
+    ssr: false,
+  }
+);
 
 export default function Home() {
   return (
-    // <AblyProvider client={client}>
-    //   <ChannelProvider channelName='main-chat'>
-    //   </ChannelProvider>
-    // </AblyProvider>
-    <div>hi lol</div>
+    <main>
+      <div>hi lol</div>
+      <Chat></Chat>
+    </main>
   );
 }
