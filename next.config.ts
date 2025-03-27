99ca10f92dc0d175d642b178d5991024f8c628f0
temp sorb852 <reeldob34@gmail.com>
2025-03-27 18:01:07 +0800

    style: style /chat

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   env: {
     ABLY_API_KEY: process.env.ABLY_API_KEY,
   },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "**",
+      },
+    ],
+    dangerouslyAllowSVG: true
+  },
 };
 
 export default nextConfig;
