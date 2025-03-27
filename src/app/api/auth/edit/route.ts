71485186261ccdaa734fb0100f53390cbcd2732c
temp sorb852 <reeldob34@gmail.com>
2025-03-27 16:50:05 +0800

    feat: teach just saying to forget it

--- src/app/api/auth/edit/route.ts
+++ src/app/api/auth/edit/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
+
 export const PUT = async (req: NextRequest) => {
   const body = await req.formData();
   console.log(body.get("file"));
