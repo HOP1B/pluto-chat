4441a85c93bc1a84061b1dfdbe4686610248ed8a
temp sorb852 <reeldob34@gmail.com>
2025-03-27 16:41:01 +0800

    feat: nothing much, commiting just in case

---
+++ src/app/api/auth/edit/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const PUT = async (req: NextRequest) => {
+  const body = await req.formData();
+  console.log(body.get("file"));
+  return NextResponse.json({ message: "hi" });
+};
