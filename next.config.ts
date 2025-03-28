f233af56c234e4d083ca15b8f485af8c25078291
= <=>
2025-03-28 17:09:05 +0800

    feat: prod

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     ABLY_API_KEY: process.env.ABLY_API_KEY,
+    DATABASE_URL: process.env.DATABASE_URL,
+    BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS,
+    JWT_SECRET: process.env.JWT_SECRET
   },
   images: {
     remotePatterns: [
